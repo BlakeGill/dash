4e57448fcfdc9a3460783f5b599b20699079e2c3
Blake Gill <bgill5@outlook.com>
2021-10-12 13:14:40 +0100

    csv test 3

--- try1.py
+++ try1.py
@@ -3,10 +3,11 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
+import plotly.express as px
 import numpy as np
 from dash.dependencies import Input, Output
 
-df = pd.read_csv('https://raw.githubusercontent.com/BlakeGill/dash/master/2021-10-08-13-48_influxdb_data.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/BlakeGill/dash/master/2021-10-12-12-57_influxdb_data.csv')
 
 app = dash.Dash()
 
@@ -14,21 +15,11 @@ app = dash.Dash()
 print(df)
 app.layout = html.Div(children=[
     html.H1(children='Hello World!'),
-    html.Div(children='Dash: a framework'),
-
-dcc.Graph(
-    id='graphid',
-    figure={
-        'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'Cats'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'Dogs'},
-            ],
-            'layout': {
-                'title': 'Cats vs Dogs'
-            }
-        }
-    )
+    html.Div(children='Dash: a framework')
 ])
+
+fig = px.line(df, x='_time', y='_value')
+fig.show()
 
 if __name__ == '__main__':
     app.run_server()
