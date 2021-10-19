d3566e482c365a19fcfdef063767c395670b52db
Blake Gill <bgill5@outlook.com>
2021-10-19 11:54:28 +0100

    multiple csvs

--- Ch001.py
+++ Ch001.py
@@ -7,21 +7,20 @@ import plotly.express as px
 import numpy as np
 from dash.dependencies import Input, Output
 
-df = pd.read_csv('https://raw.githubusercontent.com/BlakeGill/dash/master/2021-10-12-12-57_influxdb_data.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/BlakeGill/dash/master/2021-10-12-14-14_influxdb_data.csv')
 
 app = dash.Dash()
 
+values = df['false.3']
+values = values[3:]
 
-print(df)
-app.layout = html.Div(children=[
-    html.H1(children='Hello World!'),
-    html.Div(children='Dash: a framework')
-])
+time = df['false.2']
+time = time[3:]  #only using the values required
+
 #'#group', 'false', 'false.1', 'true', 'true.1', 'false.2', 'false.3', 'true.2', 'true.3'
-fig = px.line(df, x='false.3', y='true.2')
-fig.show()
+fig = px.line(df, x=time, y=values)
+fig.update_yaxes(categoryorder='category ascending', type='log', tick0=0, dtick=1)
 
-fig = go.Figure(go.Scatter(x=df['true.2'], y=df['false.3']))
 fig.show()
 
 if __name__ == '__main__':
